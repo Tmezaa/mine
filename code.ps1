a0ec6346e8d0dd04b20665022e954c435a77434f
Tmezaa <113182295+Tmezaa@users.noreply.github.com>
2023-08-02 17:07:08 -0400

    Add files via upload

--- code.ps1
+++ code.ps1
@@ -7,49 +7,45 @@ function Test-Admin {
 
 if ((Test-Admin) -eq $false)  {
     if ($elevated) {
-        # tried to elevate, did not work, aborting
+        # Tried to elevate, did not work, aborting
     } else {
         Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     }
     exit
 }
 
-'running with full privileges'
+'Running with full privileges'
 
-
-# definir le path du screensaver blank
+# Set the path of the blank screensaver
 $screensaverPath = "C:\Windows\System32\scrnsave.scr" 
 
-# change le screensaver 
+# Change the screensaver
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v "SCRNSAVE.EXE" /t REG_SZ /d $screensaverPath /f 
 
-
-# timout de 15 minutes pour afficher le screensaver
+# Set a timeout of 15 minutes to display the screensaver
 reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v ScreenSaveTimeOut /t REG_SZ /d 900 /f 
 
-# active le screensaver
+# Enable the screensaver
 reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v ScreenSaveActive /t REG_SZ /d 1 /f 
 
-# demande le password pour le screensaver
+# Require password for the screensaver
 reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop" /v ScreenSaveUsePassword /t REG_SZ /d 1 /f 
 
-# confirmer avant de vider la corbeille 
+# Confirm before emptying the Recycle Bin 
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v ConfirmFileDelete /t REG_DWORD /d 1 /f
 
-# montrer lextenssion des fichiers
+# Show file extensions
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0
 
-# montrer le hidden icons 
+# Show hidden icons 
 
-
-
-# ne pas mettre le mode sleep quand ferme le capot
+# Prevent entering sleep mode when closing the laptop lid
 powercfg -setacvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION 0
 
-# desactiver le timout pour le mode sleep quand le capot et fermer
+# Disable the timeout for entering sleep mode when the laptop lid is closed
 powercfg -setacvalueindex SCHEME_CURRENT SUB_VIDEO VIDEOIDLE 0
 
-# appliquer le changement 
+# Apply the changes 
 powercfg -SetActive SCHEME_CURRENT
 
 $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
